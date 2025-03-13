841bc7d8be5d3c95a3c27176ef954cf0812042be
DTCode <daithien2004@gmail.com>
2025-03-13 12:09:51 +0700

    sau giao dien, recyclerView

--- TheadsProject/app/src/main/java/com/example/theadsproject/PersonalDetailFragment.java
+++ TheadsProject/app/src/main/java/com/example/theadsproject/PersonalDetailFragment.java
@@ -3,17 +3,37 @@ package com.example.theadsproject;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.theadsproject.adapter.PostAdapter;
+import com.example.theadsproject.entity.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class PersonalDetailFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private List<Post> postList;
+    private PostAdapter postAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_personal_detail, container, false);
+        recyclerView = view.findViewById(R.id.rvPosts);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        postList = new ArrayList<>();
+        postList.add(new Post("lyhung_04", "5h", "Định mệnh rau muống xào măn vcl ra", "https://beptruong.edu.vn/wp-content/uploads/2021/04/rau-muong-xao-toi-1.jpg"));
+
+        postAdapter = new PostAdapter(getContext(), postList);
+        recyclerView.setAdapter(postAdapter);
+
+        return view;
     }
 }
