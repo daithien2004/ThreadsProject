05e2f6bccf09e3e2356dc96a0d5369be34cbfcf2
LyHung10 <lyhung10nctlop95@gmail.com>
2025-04-26 22:24:32 +0700

    them bio request

---
+++ threads/src/main/java/com/androidpj/threads/dto/UpdateBioRequest.java
@@ -0,0 +1,16 @@
+package com.androidpj.threads.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateBioRequest {
+    private String bio;
+}
+
